059d590c18974cdaa2861f1ac226fced3e834591
Sara Løkken <saraloekken@gmail.com>
2019-04-28 19:18:43 +0200

    Update venner.js

---
+++ src/pages/venner.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { link } from 'react-router-dom';
+import './src/style.css';
+
+export default class Venner extends Component {
+    render()   {
+        return ( 
+            <div>
+
+    
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            </div>
+        )   
+    }
+}
