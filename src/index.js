ce4a5284e0f0f0c0fecab6d6bfc259ff78a99c17
Sara Løkken <saraloekken@gmail.com>
2019-04-29 11:41:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
 import App from './app';
+import './style.css';
+import Login from './pages/login.js';
 
-
-export default class Venner extends Component {
-    render()   {
-        return ( 
-            <div>
+ReactDOM.render(
+    <App>
+        <Login />
+    </App>, 
+    document.getElementById('root')
+);
     
-            <div className="loginbox">
-                <i className="fas fa-user-cog"></i>
-                    <div className="username">
-                        <input type="text" className="input" placeholder="Brukernavn"/>
-                        <i className="fa fa-user"></i>
-                    </div>    
-                    <div className="password">
-                        <input type="password" className="input" placeholder="Passord"/>
-                        <i className="fa fa-lock"></i>
-                    </div>
-            
-                    <button type="button" className="button" >Logg inn</button>
-            
-            </div>
-        
-            
-            </div>
-        )
-    }
-}    
-    
-    
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
