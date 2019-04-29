88b0bf7dcfc054e6ac9055c6a71cd42bc15f5a57
Sara Løkken <saraloekken@gmail.com>
2019-04-29 20:49:48 +0200

    Create kontakt.js

---
+++ src/pages/kontakt.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../../src/style.css';
+
+export default class Kontakt extends Component {
+    render()   {
+        return ( 
+            <div>
+
+    
+            
+            
+            
+            
+            
+            
+            </div>
+        )   
+    }
+}
