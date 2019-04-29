1b0fa1ecccbd81859a6778d72a554d471b30c6bf
Sara Løkken <saraloekken@gmail.com>
2019-04-29 20:49:45 +0200

    Update hjem.js

---
+++ src/pages/hjem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../../src/style.css';
+
+export default class Hjem extends Component {
+    render()   {
+        return ( 
+            <div>
+
+    
+            
+            
+            
+            
+            
+            
+            </div>
+        )   
+    }
+}
