77c019517baea256c329a1ed26f5dbe0c979b528
Sara Løkken <saraloekken@gmail.com>
2019-05-01 20:00:08 +0200

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink, Route } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 class Navbar extends Component {
   render() {
@@ -8,13 +8,13 @@ class Navbar extends Component {
     <div className = "navbar">
        <i className="fal fa-users-cog"></i>
         <ul>
-            <li><NavLink exact to ="/hjem.js" activeClassName = "activeStyle"><i className="fas fa-home"></i> Hjem</NavLink></li>
-            <li><NavLink exact to ="/events.js" activeClassName = "activeStyle"><i className="fas fa-calendar-week"></i> Events</NavLink></li>
-            <li><NavLink exact to ="/historie.js" activeClassName = "activeStyle"><i className="fas fa-history"></i> Historie</NavLink></li>
-            <li><NavLink exact to ="/booking.js" activeClassName = "activeStyle"><i className="fas fa-bookmark"></i> Booking</NavLink></li>
-            <li><NavLink exact to ="/kontakt.js" activeClassName = "activeStyle"><i className="fas fa-address-book"></i> Kontakt oss</NavLink></li>
-            <li><NavLink exact to ="/venner.js" activeClassName = "activeStyle"><i className="fas fa-user-friends"></i> Våre venner</NavLink></li>
-            <li><NavLink exact to ="/logout.js" activeClassName = "activeStyle"><i className="fas fa-sign-out-alt"></i> Logg ut</NavLink></li>
+            <li><NavLink exact to ="/hjem" activeClassName = "activeStyle"><i className="fas fa-home"></i> Hjem</NavLink></li>
+            <li><NavLink exact to ="/events" activeClassName = "activeStyle"><i className="fas fa-calendar-week"></i> Events</NavLink></li>
+            <li><NavLink exact to ="/historie" activeClassName = "activeStyle"><i className="fas fa-history"></i> Historie</NavLink></li>
+            <li><NavLink exact to ="/booking" activeClassName = "activeStyle"><i className="fas fa-bookmark"></i> Booking</NavLink></li>
+            <li><NavLink exact to ="/kontakt" activeClassName = "activeStyle"><i className="fas fa-address-book"></i> Kontakt oss</NavLink></li>
+            <li><NavLink exact to ="/venner" activeClassName = "activeStyle"><i className="fas fa-user-friends"></i> Våre venner</NavLink></li>
+            <li><NavLink exact to ="/logout" activeClassName = "activeStyle"><i className="fas fa-sign-out-alt"></i> Logg ut</NavLink></li>
         </ul>    
     </div>    
         
