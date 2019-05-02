096670b8cf8cbd85f9579e7ce3fa29fb19275841
Sara Løkken <saraloekken@gmail.com>
2019-05-02 12:14:10 +0200

    Create historie.js

---
+++ src/pages/historie.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../../src/style.css';
+
+export default class Historie extends Component {
+    render()   {
+        return ( 
+            <div>
+
+    
+            
+            <div className="placeholder"></div>
+            
+            
+            
+            
+            </div>
+        )   
+    }
+}
