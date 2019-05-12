acffbccf58c7579925e83e9bec41783fbb43de19
Sara Løkken <saraloekken@gmail.com>
2019-05-12 19:29:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './components/routes';
 import App from './App';
 import './style.css';
-import Login from './pages/Login';
+import Login from './pages/login';
 
 ReactDOM.render(
     <Router>
