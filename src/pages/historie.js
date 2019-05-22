b1487aa4f3b95e28b2c26ef67c8a2ba665d3a635
Sara Løkken <saraloekken@gmail.com>
2019-05-22 20:26:01 +0200

    home

--- src/pages/historie.js
+++ src/pages/historie.js
@@ -4,16 +4,38 @@ import '../../src/style.css';
 export default class Historie extends Component {
     render()   {
         return ( 
-            <div>
-
-    
-            
-            <div className="placeholder"></div>
             
             
+            <div className="placeholder">
             
+        <div className = "story"> 
+               <h4>Ny hendelse:</h4>
+            
+                <div className="text-input">
+                    <input type="text" name='name' className="friendsinput" placeholder="Overskrift"/>            
+                </div>
+            
+            <div className ="history-info">
+            
+            <textarea placeholder="Masse kul informasjon om noe som skjedde her dette året.">
+        
+            </textarea>
             
             </div>
+            
+    
+                 <button type="button" className="addbutton"><i class="fas fa-plus"></i> Legg til</button> 
+            
+            </div>
+            
+            
+       
+            </div>
+            
+            
+            
+            
+            
         )   
     }
 }
--- src/pages/hjem.js
+++ src/pages/hjem.js
@@ -4,13 +4,42 @@ import '../../src/style.css';
 export default class Hjem extends Component {
     render()   {
         return ( 
-            <div>
+            
 
-    <div className="placeholder"></div>
-        
-    
+    <div className="placeholder">
+            
+            <div className = "banner"> 
+               <h4>Banner:</h4>
+            
+                <div className='file-input'>
+                    <input type='file'/>
+                    <span className='filebutton'>Velg</span>
+                    <span className='label' data-js-label>Bannerbilde</span>
+                </div>
+            
+                <div className="text-input">
+                    <input type="text" name='name' className="friendsinput" value="<?php echo $row['headline'];?>"/>            
+                    <input type="text" name='name' className="friendsinput" value="<?php echo $row['subtitle'];?>"/>
+                </div>
+            
+                 <button type="button" className="addbutton"><i className="fas fa-sync-alt"></i> Oppdater</button> 
+            
+            </div>
             
             
+            <div className ="contact-info"><br/><br/>
+            <h4>Kontaktinfo:</h4>
+             <div className="text-input">
+                    <input type="text" name='name' className="friendsinput" value="<?php echo $row['phoneno'];?>"/>            
+                    <input type="text" name='name' className="friendsinput" value="<?php echo $row['email'];?>"/>
+                  <input type="text" name='name' className="friendsinput" value="<?php echo $row['address'];?>"/>
+             <input type="text" name='name' className="friendsinput" value="<?php echo $row['postalcode'];?>"/>
+             <input type="text" name='name' className="friendsinput" value="<?php echo $row['place'];?>"/>
+                </div>
+            </div>
+            
+            <button type="button" className="addbutton"><i className="fas fa-sync-alt"></i> Oppdater</button>
+                   
             
             </div>
         )   
